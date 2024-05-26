ec52aae22d906f6644e0f51f11fd514e1827f6b4
Kusunoki9999 <To29Sku51@icloud.com>
2024-05-26 09:13:09 +0900

    ひな形の作成

--- public/index.php
+++ public/index.php
@@ -5,14 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex" />
-    <title>一言掲示板</title>
+    <title>ひとこと掲示板</title>
     <link rel = "stylesheet" href = "./assets/main.css"/>
 </head>
 <body>
   <div class="page-cover">
     <p class="page-title">ひとこと掲示板</p>
       <div class="form-cover">
-        <!-- 投稿内容入力フォーム -->
+        <form action="/" method="post">
+            <div class="form-input-title">投稿者ニックネーム</div>
+            <input type="text" name="author_name" maxlength="40" value="" class="input-author-name" />
+            <div class="form-input-error">
+            </div>
+            <div class="form-input-title">投稿内容<small>(必須)</small></div>
+            <textarea name="message" class="input-message"></textarea>
+            <div class="form-input-error">
+            </div>
+            <input type="hidden" name="action_type" value="insert" />
+            <button type="submit" class="input-submit-button">投稿する</button>
+        </form>
       </div>
     <hr class="page-divider" />
       <div class="message-list-cover">
