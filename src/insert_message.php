4107b8ddba340b11a5b2f0601893adbb1abc1b05
Kusunoki9999 <To29Sku51@icloud.com>
2024-05-30 09:28:12 +0900

    投稿をdbに保存する

--- src/insert_message.php
+++ src/insert_message.php
@@ -1,9 +1,10 @@
 <?php
-
+//両端の空白を除去する関数
 function mbTrim($pString)
 {
     return preg_replace('/\A[\p{Cc}\p{Cf}\p{Z}]++|[\p{Cc}\p{Cf}\p{Z}]++\z/u', '', $pString);
-} //両端の空白を除去する関数
+}
+
 
 // 投稿者の入力を確認
 $is_valid_auther_name = true;
@@ -17,6 +18,7 @@ if (isset($_POST['author_name'])) {
 if ($is_valid_auther_name && mb_strlen($input_author_name) > 30) {
     $is_valid_auther_name = false;
 } //バリデーション
+
 
 //投稿内容の入力を確認
 $is_valid_message = true;
@@ -35,3 +37,24 @@ if ($is_valid_message && mb_strlen($input_message) > 1000) {
     $is_valid_message = false;
 }
 
+// 投稿をデータベースへ保存する処理
+if ($is_valid_auther_name && $is_valid_message) {
+  if ($input_author_name === '') {
+    $input_author_name = '名無し';
+  }
+
+  // プレースホルダ(:変数)によってSQLインジェクションの対策になる！！
+  $query = 'INSERT INTO posts (author_name, message) VALUES (:author_name, :message)';
+
+  // SQL 実行の準備
+  $stmt = $dbh->prepare($query);
+
+  // プレースホルダに値をセットする
+  $stmt->bindValue(':author_name', $input_author_name, PDO::PARAM_STR);
+  $stmt->bindValue(':message', $input_message, PDO::PARAM_STR);
+
+  $stmt->execute();
+}
+
+header('Location: /');
+exit();
