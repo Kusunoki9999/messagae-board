8c3f349a16e4ef1b0ac66316249d1314024a9e2a
Kusunoki9999 <To29Sku51@icloud.com>
2024-06-05 22:52:56 +0900

    indexページ上での表示

--- public/index.php
+++ public/index.php
@@ -2,47 +2,50 @@
 session_start();
 require_once(__DIR__ . '/../src/db_connect.php');
 
-if(isset($_POST['action_type']) && $_POST['action_type']){
-  if($_POST['action_type'] === 'insert'){
+if (isset($_POST['action_type']) && $_POST['action_type']) {
+  if ($_POST['action_type'] === 'insert') {
     require(__DIR__ . '/../src/insert_message.php');
   }
 }
 
 require(__DIR__ . '/../src/session_values.php');
 ?>
-
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="noindex" />
-    <title>ひとこと掲示板</title>
-    <link rel = "stylesheet" href = "./assets/main.css"/>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="robots" content="noindex" />
+  <title>ひとこと掲示板</title>
+  <link rel="stylesheet" href="./assets/main.css" />
 </head>
+
 <body>
   <div class="page-cover">
+
     <p class="page-title">ひとこと掲示板</p>
-      <div class="form-cover">
-        <form action="/" method="post">
-            <div class="form-input-title">投稿者ニックネーム</div>
-            <input type="text" name="author_name" maxlength="40" value="" class="input-author-name" />
-            <div class="form-input-error">
-            </div>
-            <div class="form-input-title">投稿内容<small>(必須)</small></div>
-            <textarea name="message" class="input-message"></textarea>
-            <div class="form-input-error">
-            </div>
-            <input type="hidden" name="action_type" value="insert" />
-            <button type="submit" class="input-submit-button">投稿する</button>
-        </form>
-      </div>
     <hr class="page-divider" />
-      <div class="message-list-cover">
-        <!-- この中に投稿された内容のリストを表示 -->
-      </div>
+    <div class="form-cover">
+      <form action="/" method="post">
+        <div class="form-input-title">投稿者ニックネーム</div>
+        <input type="text" name="author_name" maxlength="40" value="<?php echo htmlspecialchars($messages['input_pre_author_name'], ENT_QUOTES); ?>" class="input-author-name" />
+        <?php if ($messages['input_error_author_name'] !== '') { ?>
+          <div class="form-input-error">
+            <?= $messages['input_error_author_name']; ?>
+          </div>
+        <?php } ?>
+        <div class="form-input-title">投稿内容<small>(必須)</small></div>
+        <textarea name="message" class="input-message"><?php echo htmlspecialchars($messages['input_pre_message'], ENT_QUOTES); ?></textarea>
+        <?php if ($messages['input_error_message'] !== '') { ?>
+          <div class="form-input-error">
+            <?= $messages['input_error_message']; ?>
+          </div>
+        <?php } ?>
+        <input type="hidden" name="action_type" value="insert" />
+        <button type="submit" class="input-submit-button">投稿する</button>
+      </form>
+    </div>
+
     <hr class="page-divider" />
-  </div>
-</body>
-</html>
